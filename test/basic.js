6a379dccdf5ad7d96e7b6b9ec312fa8f305561aa
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-02-07 15:38:02 -0800

    basic test of fetch fetch list functionality

--- test/basic.js
+++ test/basic.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const chai = require('chai');
+
 const ml2on = require('../lib/ml2on.js');
 const duh2spirit = require('../lib/duh2spirit.js');
+const ipxactSchemas = require('../lib/ipxact-schemas.js');
 
 const expect = chai.expect;
 
@@ -15,6 +17,11 @@ describe('basic', () => {
 
   it('duh2spirit is function', done => {
     expect(duh2spirit).to.be.a('function');
+    done();
+  });
+
+  it('ipxactSchemas is array', done => {
+    expect(ipxactSchemas).to.be.a('array');
     done();
   });
 
